a7ee1e394250e408ec1b1d9bbb8c1c0274e9fd1d
baozhoutao <baozhoutao@hotoa.com>
2025-03-28 17:07:38 +0800

    上下文加上scoped

--- packages/react/src/blocks/Amis.tsx
+++ packages/react/src/blocks/Amis.tsx
@@ -99,8 +99,10 @@ export class AmisComponent extends React.Component<PropsWithChildren<AmisProps>>
       ...this.props.env
     };
     console.log('render amis', this.props, data, env);
-    this.amisScoped = this.amis.embed(this.ref.current, this.props.schema, {data}, env);
-    Object.assign(this._scoped, this.amisScoped);
+    this.amisScoped = this.amis.embed(this.ref.current, this.props.schema, {data}, env, ()=>{
+      console.log('this.amisScoped', this.amisScoped)
+      Object.assign(this._scoped, this.amisScoped);
+    });
   }
 
   async componentDidUpdate(prevProps) {
